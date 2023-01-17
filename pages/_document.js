e3e13501966ca9ea326322227761bedb9ecd26e6
Roq4 <ocakkoc@gmail.com>
2023-01-17 14:05:30 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-200 via-gray-400 to-gray-600">
+      <body className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-200 via-gray-400 to-gray-600 h-screen">
         <Main />
         <NextScript />
       </body>
